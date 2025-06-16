d375564989c36bc565b58237350c2f318159689c
gaoxin <AshinGau@outlook.com>
2025-06-16 21:55:19 +0800

    nested hash

--- bin/gravity_node/src/main.rs
+++ bin/gravity_node/src/main.rs
@@ -127,12 +127,7 @@ fn run_reth(
                     reth_transaction_pool::blobstore::DiskFileBlobStore,
                 > = handle.node.pool;
 
-                let storage = BlockViewStorage::new(
-                    provider.clone(),
-                    latest_block.number,
-                    latest_block_hash,
-                    BTreeMap::new(),
-                );
+                let storage = BlockViewStorage::new(provider.clone());
                 let pipeline_api_v2: PipeExecLayerApi<BlockViewStorage<BlockchainProvider<NodeTypesWithDBAdapter<EthereumNode, Arc<DatabaseEnv>>>>> = reth_pipe_exec_layer_ext_v2::new_pipe_exec_layer_api(
                     chain_spec,
                     storage,
